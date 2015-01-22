6348f09d3c53374e9e416b3bebe4b428abf09a3a
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-22 09:59:15 -0500

    adds question_2 javascript solution

---
+++ question_2.js
@@ -0,0 +1,14 @@
+fibSeed = [1,2];
+var sum = 2;
+
+while(fibSeed[fibSeed.length-1] < 4000000){
+  var nextFib = fibSeed[fibSeed.length-1] + fibSeed[fibSeed.length-2]
+
+  fibSeed.push(nextFib);
+
+  if(nextFib % 2 === 0)
+    sum += nextFib;
+
+}
+
+console.log(sum);
